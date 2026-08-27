b6b82f9a525f61bfe69aae032ce7185503df7c13
devarturobl <4rthurbl00@gmail.com>
2026-08-27 10:08:13 -0600

    Dia 2 Clase 2

--- semana1/main.py
+++ semana1/main.py
@@ -3,20 +3,29 @@ import streamlit as st
 # Titulo del app web
 st.title("Este es mi primer codigo de Streamlit")
 st.header("Primer Vista de APP WEB")
-st.subheader("Usando Rerun")
+st.subheader("Consulta el repositorio: https://github.com/devarturobl/programacionweb26")
 
 st.badge("New")
 st.badge("Success", icon="🤖", color="green")
 
 def sumar(a, b):
-    return a + b
+    resultado = int(a) + int(b)
+    return st.write(resultado)
 
-resultado = sumar(50,25)
 
-st.write(resultado)
 
 def mensaje(mimensaje):
     for a in range(10):
         st.text(mimensaje)
 
-mensaje("Hola esto funciona")
+st.header("Usando Input Widgets")
+st.subheader("Botones")
+
+if st.button("Saludar"):
+    mensaje("Hola Clase")
+
+dato1 = st.text_input("Introduce el numero 1")
+dato2 = st.text_input("Introduce el nuemero 2")
+
+if st.button("Sumar"):
+    sumar(dato1,dato2)
